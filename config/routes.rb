c915198262a7b85d0e4d2c586dff53f4a161ae19
tiger <ftaigasin1998@gmail.com>
2019-04-20 08:49:44 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create, :edit, :update] do
+  resources :users, only: [:index, :show, :new, :create, :edit, :update] do
     member do
       get :followings
       get :followers
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
   end
   
   resources :menus, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  
 end
